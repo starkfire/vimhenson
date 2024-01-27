adb3100aaf9fd51e6175c544d479ac6de3b19672
starkfire <plpantoja49@gmail.com>
2024-01-27 14:09:41 +0800

    feat: navigate between splits using <C-[h/j/k/l]>

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -11,3 +11,9 @@ map('i', '<', '<><Esc>ha')
 map('i', '"', '""<Esc>ha')
 map('i', "'", "''<Esc>ha")
 map('i', "`", "``<Esc>ha")
+
+-- navigate between splits
+map('n', "<c-k>", ":wincmd k<CR>", { noremap = true, silent = true })
+map('n', "<c-j>", ":wincmd j<CR>", { noremap = true, silent = true })
+map('n', "<c-h>", ":wincmd h<CR>", { noremap = true, silent = true })
+map('n', "<c-l>", ":wincmd l<CR>", { noremap = true, silent = true })
