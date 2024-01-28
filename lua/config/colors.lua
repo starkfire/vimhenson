51f57482dcab00521ca67723b454b773b7aec7c1
starkfire <plpantoja49@gmail.com>
2024-01-28 21:46:45 +0800

    feat: add colors config

---
+++ lua/config/colors.lua
@@ -0,0 +1,14 @@
+local colors = {
+    main = {
+        tokyonight_color = "#2d3149",
+        bg_dark = "#16161e",
+        text_color = "#131a21"
+    },
+    bufferline = {
+        background = "#16161e",
+        active = "#d183e8",
+        inactive = "#303030"
+    }
+}
+
+return colors
