f253ac21d5d6dc5e8fcc098316f16d4f91ae10b2
starkfire <plpantoja49@gmail.com>
2024-01-28 13:38:00 +0800

    feat: add mappings for telescope and bufferline

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -23,3 +23,29 @@ map('n', "<leader><Up>", ":wincmd k<CR>", { noremap = true, silent = true })
 map('n', "<leader><Down>", ":wincmd j<CR>", { noremap = true, silent = true })
 map('n', "<leader><Left>", ":wincmd h<CR>", { noremap = true, silent = true })
 map('n', "<leader><Right>", ":wincmd l<CR>", { noremap = true, silent = true })
+
+-- buffer navigation via bufferline
+map('n', "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>")
+map('n', "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>")
+map('n', "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>")
+map('n', "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>")
+map('n', "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>")
+map('n', "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>")
+map('n', "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>")
+map('n', "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>")
+map('n', "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>")
+map('n', "<leader>$", "<Cmd>BufferLineGoToBuffer -1<CR>")
+
+-- Telescope
+local telescope_path = vim.fn.stdpath("data") .. "/lazy/telescope.nvim"
+
+if vim.loop.fs_stat(telescope_path) then
+    vim.opt.rtp:prepend(telescope_path)
+
+    local builtin = require('telescope.builtin')
+
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+end
