0ecf75b2d04ffcfb5b37063bfcd93aeb1bfaa1d0
starkfire <plpantoja49@gmail.com>
2024-01-31 01:07:28 +0800

    feat: add bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+local bufferline_config = {
+    options = {
+        numbers = "ordinal",
+        diagnostics = "coc", -- change to "nvim_lsp" if not using CoC
+        diagnostics_update_in_insert = true,
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local s = ""
+
+            for e, n in pairs(diagnostics_dict) do
+                local sym = e == "error" and " "
+                    or (e == "warning" and " " or "" )
+
+                s = s .. n .. sym
+            end
+
+            return s
+        end
+    }
+}
+
+return {
+    'akinsho/bufferline.nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons'
+    },
+    --[[config = function()
+        require('bufferline').setup(bufferline_config)
+    end]]--
+}
