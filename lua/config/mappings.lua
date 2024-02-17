32bcd04647a0fe581a3c8c2f76871c4b1e996d14
starkfire <plpantoja49@gmail.com>
2024-02-17 23:19:48 +0800

    refactor: move cokeline mappings to plugin config

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -24,23 +24,6 @@ map('n', "<leader><Down>", ":wincmd j<CR>", { noremap = true, silent = true })
 map('n', "<leader><Left>", ":wincmd h<CR>", { noremap = true, silent = true })
 map('n', "<leader><Right>", ":wincmd l<CR>", { noremap = true, silent = true })
 
--- buffer navigation via bufferline
-local bufferline_path = vim.fn.stdpath("data") .. "/lazy/nvim-cokeline"
-
-if vim.loop.fs_stat(bufferline_path) then
-    vim.opt.rtp:prepend(bufferline_path)
-
-    for i = 1, 9 do
-        map(
-            'n',
-            ("<Leader>%s"):format(i),
-            ("<Cmd>BufferLineGoToBuffer %s<CR>"):format(i)
-        )
-    end
-
-    map('n', "<Leader>$", "<Cmd>BufferLineGoToBuffer -1<CR>")
-end
-
 -- Telescope
 local telescope_path = vim.fn.stdpath("data") .. "/lazy/telescope.nvim"
 
@@ -53,29 +36,4 @@ if vim.loop.fs_stat(telescope_path) then
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-end
-
--- Cokeline
-local cokeline_path = vim.fn.stdpath("data") .. "/lazy/nvim-cokeline"
-
-if vim.loop.fs_stat(cokeline_path) then
-    vim.opt.rtp:prepend(cokeline_path)
-
-    map('n', "<S-Tab>", "<Plug>(cokeline-focus-prev)")
-    map('n', "<Tab>", "<Plug>(cokeline-focus-next)")
-    map('n', "<Leader>p", "<Plug>(cokeline-switch-prev)")
-    map('n', "<Leader>n", "<Plug>(cokeline-switch-next)")
-
-    for i = 1, 9 do
-        map(
-            'n',
-            ("<Leader>%s"):format(i),
-            ("<Plug>(cokeline-focus-%s)"):format(i)
-        )
-        map(
-            'n',
-            ("<F%s>"):format(i),
-            ("<Plug>(cokeline-switch-%s)"):format(i)
-        )
-    end
 end
--- lua/plugins/cokeline.lua
+++ lua/plugins/cokeline.lua
@@ -1,7 +1,32 @@
+local util = require("modules.util")
 local cokeline_path = vim.fn.stdpath("data") .. "/lazy/nvim-cokeline"
 
 if vim.loop.fs_stat(cokeline_path) then
     vim.opt.rtp:prepend(cokeline_path)
+
+    -- switch between tabs
+    map('n', "<S-Tab>", "<Plug>(cokeline-focus-prev)")
+    map('n', "<Tab>", "<Plug>(cokeline-focus-next)")
+
+    -- switch tab order
+    map('n', "<Leader>p", "<Plug>(cokeline-switch-prev)")
+    map('n', "<Leader>n", "<Plug>(cokeline-switch-next)")
+
+    for i = 1, 9 do
+        -- switch between tabs
+        map(
+            'n',
+            ("<Leader>%s"):format(i),
+            ("<Plug>(cokeline-focus-%s)"):format(i)
+        )
+
+        -- switch tab order
+        map(
+            'n',
+            ("<F%s>"):format(i),
+            ("<Plug>(cokeline-switch-%s)"):format(i)
+        )
+    end
 end
 
 local colors = require('config.colors')
