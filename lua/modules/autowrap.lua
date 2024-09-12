e30c728dbf1857143eebfd061f3905671ae487d0
starkfire <plpantoja49@gmail.com>
2024-09-13 00:31:51 +0800

    fix: wrap function should work on multi-line selections

--- lua/modules/autowrap.lua
+++ lua/modules/autowrap.lua
@@ -6,29 +6,27 @@
 local function wrap_text(opening, closing)
     local s_pos = vim.fn.getpos("v")
     local e_pos = vim.fn.getpos(".")
+    local bufnr = vim.api.nvim_get_current_buf()
 
     -- swap positions when the direction of selection is backwards
     if (s_pos[2] > e_pos[2]) or ((s_pos[2] == e_pos[2]) and (s_pos[3] > e_pos[3])) then
         s_pos, e_pos = e_pos, s_pos
     end
 
-    local selected_text = vim.api.nvim_buf_get_text(s_pos[1], s_pos[2] - 1, s_pos[3] - 1, e_pos[2] - 1, e_pos[3], {})
-    local wrapped_text = opening .. table.concat(selected_text, "\n") .. closing
+    local selected_text = vim.api.nvim_buf_get_text(bufnr, s_pos[2] - 1, s_pos[3] - 1, e_pos[2] - 1, e_pos[3] - 1, {})
 
-    vim.api.nvim_buf_set_text(s_pos[1], s_pos[2] - 1, s_pos[3] - 1, e_pos[2] - 1, e_pos[3], { wrapped_text })
-end
+    if #selected_text == 1 then
+        selected_text[1] = opening .. selected_text[1] .. closing
+    else
+        selected_text[1] = opening .. selected_text[1]
+        selected_text[#selected_text] = selected_text[#selected_text] .. closing
+    end
 
-local function wrap_visual(wrapper)
-    wrap_text(wrapper, wrapper)
-end
-
-local function wrap_paired_visual(opening, closing)
-    wrap_text(opening, closing)
+    vim.api.nvim_buf_set_text(bufnr, s_pos[2] - 1, s_pos[3] - 1, e_pos[2] - 1, e_pos[3] - 1, selected_text)
 end
 
 local autowrap = {
-    wrap = wrap_visual,
-    wrap_paired = wrap_paired_visual
+    wrap = wrap_text,
 }
 
 return autowrap
