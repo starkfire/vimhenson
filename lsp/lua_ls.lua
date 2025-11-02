4afc510bd4821807ec9a96976e4e5140105a80a3
Pocholo Pantoja <plpantoja49@gmail.com>
2025-11-02 15:42:44 +0800

    refactor: load runtime file + deep-merge defaults

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,23 @@
+return {
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+            return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                version = 'LuaJIT'
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME
+                }
+            }
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
+}
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -44,154 +44,34 @@ return {
 
             -- diagnostics
             vim.keymap.set('n', '<C-K>', vim.diagnostic.open_float, attach_opts)
-
-            vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-                vim.lsp.diagnostic.on_publish_diagnostics,
-                {
-                    underline = true,
-                    virtual_text = false,
-                    signs = true,
-                    update_in_insert = false
-                }
-            )
         end
 
-        -- Lua
-        vim.lsp.config('lua_ls', {
-            on_init = function(client)
-                local path = client.workspace_folders[1].name
-                if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
-                    return
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                    runtime = {
-                        version = 'LuaJIT'
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME
-                        }
-                    }
-                })
-            end,
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {}
-            }
+        -- configure the vim.diagnostic API globally
+        vim.diagnostic.config({
+            underline = true,
+            virtual_text = false,
+            signs = true,
+            update_in_insert = false
         })
-        vim.lsp.enable('lua_ls')
+        
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.diagnostic.on_publish_diagnostics,
+            diagnostic_opts
+        )
 
-        -- Python
-        vim.lsp.config('pyright', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            settings = {
-                python = {
-                    analysis = {
-                        autoSearchPaths = true,
-                        useLibraryCodeForTypes = true,
-                        diagnosticMode = 'openFilesOnly'
-                    }
-                }
-            }
-        })
-        vim.lsp.enable('pyright')
+        -- configure servers and use defaults
+        for _, name in ipairs(servers) do
+            local ok, server_opts = pcall(require, 'lsp.' .. name)
+            
+            if not ok then
+                server_opts = {}
+            end
 
-        -- C / C++ / CUDA / ObjC / ObjCpp / Proto
-        vim.lsp.config('clangd', {
-            on_attach = on_attach,
-            capabilities = capabilities
-        })
-        vim.lsp.enable('clangd')
+            local opts = vim.tbl_deep_extend('force', { on_attach = on_attach, capabilities = capabilities }, server_opts)
 
-        -- Golang
-        vim.lsp.config('gopls', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('gopls')
-
-        -- Zig
-        vim.lsp.config('zls', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('zls')
-
-        -- Elixir
-        vim.lsp.config('elixirls', {
-            filetypes = { "ex", "exs", "elixir", "eelixir", "heex", "surface" },
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('elixirls')
-
-        -- Nim
-        vim.lsp.config('nim_langserver', {
-            filetypes = { "nim" },
-            on_attach = on_attach,
-            capabilities = capabilities
-        })
-        vim.lsp.enable('nim_langserver')
-
-        -- JS/TS + Vue Language Server
-        local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
-
-        vim.lsp.config('ts_ls', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            init_options = {
-                plugins = {
-                    {
-                        name = '@vue/typescript-plugin',
-                        location = vue_language_server_path,
-                        languages = { 'vue' },
-                    },
-                },
-            },
-            filetypes = {
-                "javascript",
-                "typescript",
-                "javascriptreact",
-                "javascript.jsx",
-                "typescriptreact",
-                "typescript.tsx",
-                "vue"
-            },
-        })
-        vim.lsp.enable('ts_ls')
-
-        -- Vue
-        vim.lsp.config('volar', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('volar')
-
-        -- Haskell
-        vim.lsp.config('hls', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-            filetypes = { "haskell", "lhaskell", "cabal" }
-        })
-        vim.lsp.enable('hls')
-
-        -- Gleam
-        vim.lsp.config('gleam', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('gleam')
-
-        -- JSON
-        vim.lsp.config('jsonls', {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('jsonls')
+            vim.lsp.config(name, opts)
+            vim.lsp.enable(name)
+        end
 
     end
 }
