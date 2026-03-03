488617f3f097d46146677993fea3e378f451a4a9
Pocholo Pantoja <plpantoja49@gmail.com>
2026-03-04 07:43:38 +0800

    feat: extend keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,19 @@ return {
             vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+            -- LSP
+            vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+            vim.keymap.set('n', '<leader>fS', builtin.lsp_workspace_symbols, {})
+            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+
+            -- help
             vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+            -- Git
+            vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+            vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+            vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
         end
 
         return {
